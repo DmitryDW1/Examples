0ea572720d63b8232f42b5dbf4c593b6833b5b87
DmitryDW1 <118869480+DmitryDW1@users.noreply.github.com>
2022-12-16 22:50:48 +0600

    update Program.cs

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,11 @@
-﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿using System;
+public class Sum {
+    public static void Main() {
+        int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+        Console.WriteLine(numberA);
+        int numberB = new Random().Next(1, 10);
+        Console.WriteLine(numberB);
+        int result = numberA + numberB;
+        Console.WriteLine(result);
+    }
+}
